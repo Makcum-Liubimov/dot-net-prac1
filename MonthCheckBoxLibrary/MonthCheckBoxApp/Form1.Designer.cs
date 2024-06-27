b96c858563e1f9a89dd631979e99c2d49bd18646
Makcum-Liubimov <max.liubimov@gmail.com>
2024-06-27 16:34:29 +0300

    added vs code
    vs code of original app

---
+++ MonthCheckBoxLibrary/MonthCheckBoxApp/Form1.Designer.cs
@@ -0,0 +1,28 @@
+﻿namespace MonthCheckBoxApp
+{
+    partial class Form1
+    {
+        private void InitializeComponent()
+        {
+            this.monthCheckBoxGroup = new MonthCheckBoxLibrary.MonthCheckBoxGroup();
+            this.SuspendLayout();
+            // 
+            // monthCheckBoxGroup
+            // 
+            this.monthCheckBoxGroup.Location = new System.Drawing.Point(12, 12);
+            this.monthCheckBoxGroup.Name = "monthCheckBoxGroup";
+            this.monthCheckBoxGroup.Size = new System.Drawing.Size(200, 400);
+            this.monthCheckBoxGroup.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.monthCheckBoxGroup);
+            this.Name = "Form1";
+            this.Text = "Month CheckBox App";
+            this.ResumeLayout(false);
+        }
+
+        private MonthCheckBoxLibrary.MonthCheckBoxGroup monthCheckBoxGroup;
+    }
+}
---
+++ MonthCheckBoxLibrary/MonthCheckBoxApp/Form1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Windows.Forms;
+using MonthCheckBoxLibrary;
+
+namespace MonthCheckBoxApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
